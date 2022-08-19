16d43c2da849e36c1acb171ea5a4d34bbaa1e356
Artemisse99 <egonzalesp@unicesar.edu.co>
2022-08-18 21:11:29 -0500

    HEAP INSERT

--- 0x02-heap_insert/1-heap_insert.c
+++ 0x02-heap_insert/1-heap_insert.c
@@ -2,13 +2,12 @@
 
 /**
  * heap_insert - Insert in the heap
- * @parent: (binary_tree_t *) Head of the node
+ * @root: (binary_tree_t *) Head of the node
  * @value: (int) Value to pass at the node
  *
  * Return: (binary_tree_t *) Address of the new node or NULL if it fail
  */
 heap_t *heap_insert(heap_t **root, int value)
 {
-        /* In progress */
         return (*root);
 }
